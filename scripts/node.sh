0dfbe04e8d8d7b7251299067d74f9c294c81b360
Billy- <billy@billym.co.uk>
2018-05-15 19:39:45 +0100

    Fix yarn install

--- scripts/node.sh
+++ scripts/node.sh
@@ -19,7 +19,7 @@ fi
 if [[ -z "$(which yarn)" ]]; then
   # Install yarn
   echo "Installing yarn..."
-  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  wget -qO- https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
   sudo apt-get update && sudo apt-get install yarn
 else
